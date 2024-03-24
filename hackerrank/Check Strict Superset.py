c63d4da3d9cb61ff1750b509645398437b13983e
enaassk <114976417+enaassk@users.noreply.github.com>
2024-03-24 22:08:44 +0600

    Delete hackerrank directory

--- hackerrank/Check Strict Superset.py
+++
@@ -1,9 +0,0 @@
-setA = set(input().split())
-superset = True
-
-for _ in range(int(input())):
-    if not setA.issuperset(input().split()):
-        superset = False
-        break
-        
-print(superset)
